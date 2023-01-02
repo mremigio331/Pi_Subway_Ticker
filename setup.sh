032eab406bcfd2f5af50b93417572e41537f5d90
Matthew Remigio <m.remigio331@gmail.com>
2023-01-01 17:32:24 -0800

    Creates a setup.sh file that follows setup guide at https://howchoo.com/pi/raspberry-pi-led-matrix-panel

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pip install -r requirements.txt
+cd
+sudo apt-get update && sudo apt-get install -y git python3-dev python3-pillow
+git clone https://github.com/hzeller/rpi-rgb-led-matrix.git
+cd rpi-rgb-led-matrix
+make build-python PYTHON=$(which python3)
+sudo make install-python PYTHON=$(which python3)
+cd bindings/python/samples
+sudo python3 runtext.py --led-cols=64 --led-gpio-mapping=adafruit-hat --text="Hello, world!"
