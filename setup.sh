1c5d2d203e5489d7a4ea89e107c5e5bed90c48ff
Matthew Remigio <m.remigio331@gmail.com>
2024-04-28 07:37:16 +0100

    Updating update.sh to change permissions and adding the proper apiEndpoint

--- setup.sh
+++ setup.sh
@@ -10,8 +10,11 @@ sudo apt install python3-protobuf -y
 sudo apt install python3-requests -y
 sudo apt install python3-flask -y
 pip3 install flask-cors --break-system-packages
+pip3 install flask_caching --break-system-packages
 pip install --upgrade google-api-python-client
 pip install --upgrade gtfs-realtime-bindings
+chmod +x backend/pi_local_api.py
+echo -e "//edited from initial setup\nexport const apiEndpoint = '$HOSTNAME.local'" > frontend/src/configs/apiConfig.js 
 cd
 sudo apt-get install -y git python3-dev python3-pillow
 git clone https://github.com/hzeller/rpi-rgb-led-matrix.git
